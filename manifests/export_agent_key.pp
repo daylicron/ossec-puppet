338303a12de0c6e5732d9fa614a628fdb059bb6d
Dirk Roeder <d.roeder@babiel.com>
2018-06-28 11:45:06 +0200

    Adding seed for random function

--- manifests/export_agent_key.pp
+++ manifests/export_agent_key.pp
@@ -1,13 +1,13 @@
 #Export agent key
 class ossec::export_agent_key($max_clients, $agent_name, $agent_ip_address) {
   ossec::agentkey{ "ossec_agent_${agent_name}_client":
-    agent_id         => fqdn_rand($max_clients),
+    agent_id         => fqdn_rand($max_clients, $::fqdn),
     agent_name       => $agent_name,
     agent_ip_address => $agent_ip_address,
   }
 
   @@ossec::agentkey{ "ossec_agent_${agent_name}_server":
-    agent_id         => fqdn_rand($max_clients),
+    agent_id         => fqdn_rand($max_clients, $::fqdn),
     agent_name       => $agent_name,
     agent_ip_address => $agent_ip_address
   }
